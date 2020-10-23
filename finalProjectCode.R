33f11faee683f4cbf564d27d4e3e6c380c654929
Julia Yanes <jyanes99@gmail.com>
2020-10-23 16:29:42 +0100

    Edit surname to Julia

--- finalProjectCode.R
+++ finalProjectCode.R
@@ -1,5 +1,5 @@
 #' ---
 #' title: Are state populations proportional to total covid-19 cases of the state thereof?
-#' author: Aidan Dunleavy, Julia Surname
+#' author: Aidan Dunleavy, Julia Yanes Barrera
 #' date: 10/23/2020
 #' ---
